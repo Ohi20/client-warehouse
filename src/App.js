ca0aa413d36b2f7d51278dafd0d72d12179d1253
Ohi97 <ashrafohi20@gmail.com>
2022-06-02 02:56:57 +0600

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs';
+import Home from './Pages/Home/Home';
+import Inventory from './Pages/Inventoryitems/Inventory';
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/inventory" element={<Inventory></Inventory>} />
+        <Route path="/blogs" element={<Blogs></Blogs>} />
+        
+      </Routes>
     </div>
   );
 }
